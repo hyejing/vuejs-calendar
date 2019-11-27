d60b43742937f63429166a3f5898b9e6f91b524a
hyejing <aksdir2108@naver.com>
2019-11-27 09:54:06 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class='inputBox shadow'>
+        <input type='text' v-model="newDateYear">년
+        <input type='text' v-model="newDateMonth">월
+        <input type='text' v-model="newDateDay">일
+       일정 :<input type='text' v-model="newTodoItem" v-on:keyup.enter="addTodo">
+       <span class="addContainer"  v-on:click="addTodo">
+        +
+        </span>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data:function(){
+        return{
+            newTodoItem: "",
+            newDateYear: "",
+            newDateMonth: "",
+            newDateDay: ""
+        }
+    },
+   methods:{
+        addTodo: function(){
+            var obj = {completed: false, year: this.newDateYear, month: this.newDateMonth,day: this.newDateDay,item: this.newTodoItem};
+            //localStorage.setItem(this.newTodoItem, obj);
+            localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+            //localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput:function(){
+               this.newTodoItem = '';
+               this.newDateYear = '';
+               this.newDateMonth = '';
+               this.newDateDay = '';
+        }
+    },
+    components:{
+      
+    }
+}
+</script>
+
+<style scoped>
+input:focus{
+    outline:none;
+}
+.inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input{
+    border-style:none;
+    font-size:0.9rem;
+    background-color: cornsilk;
+}
+
+.addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display:block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn{
+    color: white;
+    vertical-align: middle;
+}
+.closeModalBtn{
+    color: #42b983
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+                <div v-bind:class="{checkBtnCompleted: todoItem.completed}"  v-on:click="toggleComplete(todoItem, index)">✔</div>
+                <span v-bind:class="{textCompleted:todoItem.completed}">{{ todoItem.item }}</span>
+                <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+                <div>X</div>
+                </span>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            todoItems:[]
+        }
+    },
+    methods:{
+        removeTodo:function(todoItem,index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem.item);
+            this.todoItems.splice(index,1);
+        },
+        toggleComplete: function(todoItem,index){
+            todoItem.completed = !todoItem.completed;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+        }
+    },
+    created: function(){
+        if(localStorage.length >0){
+            for(var i = 0 ; i < localStorage.length ; i++)
+            {
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+                {
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin:0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.removeBtn{
+    margin-left: auto;
+    color:#de4343;
+}
+.checkBtn{
+    line-height: 45px;
+    color:#62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted{
+    color:#b3adad;
+}
+.textCompleted{
+    text-decoration: line-through;
+    color:#b3adad;
+}
+
+.list-enter-active, .list-leave-active{
+    transition: all 1s;
+}
+.list-enter, .list-leave-to{
+    opacity:0;
+    transform: translateY(30px);
+}
+
+</style>
